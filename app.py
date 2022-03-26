9735832ab701eead6bbae53284ceebb360fdcc16
Ranjan <skillsapphire@gmail.com>
2022-03-26 23:52:34 +0100

    new changes

--- app.py
+++ app.py
@@ -1,10 +1,59 @@
-from flask import Flask
+from flask import Flask, json, request, jsonify
+import os
+import urllib.request
+from werkzeug.utils import secure_filename
+from bank.bank_xl_xml_transaction import generateTnxXml
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello World!"
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+ALLOWED_EXTENSIONS = set(['xlsx'])
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/')
+def main():
+    return 'Homepage'
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    files = request.files.getlist('files[]')
+    errors = {}
+    success = False
+
+    for file in files:
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            success = True
+        else:
+            errors[file.filename] = 'File type is not allowed'
+
+    if success and errors:
+        errors['message'] = 'File(s) successfully uploaded'
+        resp = jsonify(errors)
+        resp.status_code = 500
+        return resp
+    if success:
+        resp = jsonify({'message': 'Files successfully uploaded'})
+        resp.status_code = 201
+        return resp
+    else:
+        resp = jsonify(errors)
+        resp.status_code = 500
+        return resp
+
+
+@app.route('/xml')
+def getXml():
+    return generateTnxXml()
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ bank/bank_xl_xml_transaction.py
@@ -0,0 +1,205 @@
+from openpyxl import load_workbook#pip install openpyxl
+from yattag import Doc, indent#pip install yattag
+from datetime import datetime
+import uuid
+from random import randint
+import re
+
+def random_with_N_digits(n):
+    range_start = 10 ** (n - 1)
+    range_end = (10 ** n) - 1
+    return randint(range_start, range_end)
+
+def generateTnxXml():
+    fileName = "Final_Excel_Format_Bank_V1"
+    path = "static/uploads/"+fileName+".XLSX"
+    wb = load_workbook(path, data_only=True)
+    ws = wb.active
+
+    min_row = ws.min_row
+    max_row = ws.max_row
+    min_col = ws.min_column
+    max_col = ws.max_column
+
+    sales_row_list:list = []
+    # Will print a particular row value
+    for row in ws.iter_rows(min_row=3, max_row=ws.max_row, min_col=2, max_col=ws.max_column):
+        row = [cell.value for cell in row]
+        sales_row_list.append(row)
+
+    print(sales_row_list)
+    doc, tag, text = Doc().tagtext()
+
+    with tag("ENVELOPE"):
+        with tag("HEADER"):
+            with tag("TALLYREQUEST"):
+                text("Import Data")
+        with tag("BODY"):
+            with tag("IMPORTDATA"):
+                with tag("REQUESTDESC"):
+                    with tag("REPORTNAME"):
+                        text("All Masters")
+                    with tag("STATICVARIABLES"):
+                        with tag("SVCURRENTCOMPANY"):
+                            text(fileName)
+                with tag("REQUESTDATA"):
+                        #Tally message gets repeated
+                        #Start of One Tally message start
+                    for row in sales_row_list:
+                        with tag("TALLYMESSAGE", ("xmlns:UDF","TallyUDF")):
+                            #Start of Voucher
+                            with tag("VOUCHER",
+                                     ("REMOTEID", str(uuid.uuid4())),
+                                     ("VCHKEY", str(uuid.uuid4())+":00000008"),
+                                     ("VCHTYPE", row[7]),
+                                     ("ACTION", "Create"),
+                                     ("OBJVIEW", "Accounting Voucher View")
+                                     ):
+                                with tag("OLDAUDITENTRYIDS.LIST",
+                                             ("TYPE", "Number")):
+                                    with tag("OLDAUDITENTRYIDS"):
+                                        text(int(-1))
+                                with tag("DATE"):
+                                    saleDate = row[4]
+                                    #print(saleDate.strftime("%Y%m%d"))
+                                    formatedDate = "NA"
+                                    if(saleDate):
+                                        formatedDate = saleDate.strftime("%Y%m%d")
+                                    text(str(formatedDate))
+                                with tag("GUID"):
+                                    text(str(uuid.uuid4()))
+                                with tag("NARRATION"):
+                                    text(str(row[6]))
+                                with tag("PARTYLEDGERNAME"):
+                                    text(str(row[3]))
+                                with tag("VOUCHERTYPENAME"):
+                                    text(row[7])
+                                with tag("VOUCHERNUMBER"):
+                                    text("1")
+                                with tag("CSTFORMISSUETYPE"):
+                                    text("")
+                                with tag("CSTFORMRECVTYPE"):
+                                    text("")
+                                with tag("FBTPAYMENTTYPE"):
+                                    text("Default")
+                                with tag("PERSISTEDVIEW"):
+                                    text("Accounting Voucher View")
+                                with tag("VCHENTRYMODE"):
+                                    text("Double Entry")
+                                with tag("EFFECTIVEDATE"):
+                                    saleDate = row[4]
+                                    # print(saleDate.strftime("%Y%m%d"))
+                                    formatedDate = "NA"
+                                    if (saleDate):
+                                        formatedDate = saleDate.strftime("%Y%m%d")
+                                    text(str(formatedDate))
+                                with tag("HASCASHFLOW"):
+                                    text("Yes")
+                                with tag("ISVATDUTYPAID"):
+                                    text("Yes")
+                                with tag("ALTERID"):
+                                    text("867")
+                                with tag("ISVATDUTYPAID"):
+                                    text("433")
+                                with tag("VOUCHERKEY"):
+                                    text(random_with_N_digits(15))
+                                with tag("ALLLEDGERENTRIES.LIST"):
+                                    with tag("OLDAUDITENTRYIDS.LIST",
+                                             ("TYPE", "Number")):
+                                        with tag("OLDAUDITENTRYIDS"):
+                                            text(int(-1))
+                                    with tag("LEDGERNAME"):
+                                        if re.search("Receipt", row[7], re.IGNORECASE):
+                                            text(row[2])
+                                        elif re.search("Payment", row[7], re.IGNORECASE):
+                                            text(row[0])
+                                    with tag("ISPARTYLEDGER"):
+                                        text("Yes")
+                                    with tag("AMOUNT"):
+                                        if re.search("Receipt", row[7], re.IGNORECASE):
+                                            text(round(row[5], 2))
+                                        elif re.search("Payment", row[7], re.IGNORECASE):
+                                            text(-round(row[5], 2))
+                                with tag("ALLLEDGERENTRIES.LIST"):
+                                    with tag("OLDAUDITENTRYIDS.LIST",
+                                             ("TYPE", "Number")):
+                                        with tag("OLDAUDITENTRYIDS"):
+                                            text(int(-1))
+                                    with tag("LEDGERNAME"):
+                                        if re.search("Receipt", row[7], re.IGNORECASE):
+                                            text(row[0])
+                                        elif re.search("Payment", row[7], re.IGNORECASE):
+                                            text(row[2])
+                                    with tag("ISDEEMEDPOSITIVE"):
+                                        text("Yes")
+                                    with tag("ISPARTYLEDGER"):
+                                        text("Yes")
+                                    with tag("ISLASTDEEMEDPOSITIVE"):
+                                        text("Yes")
+                                    with tag("AMOUNT"):
+                                        if re.search("Receipt", row[7], re.IGNORECASE):
+                                            text(-round(row[5], 2))
+                                        elif re.search("Payment", row[7], re.IGNORECASE):
+                                            text(round(row[5], 2))
+                                    with tag("BANKALLOCATIONS.LIST"):
+                                        with tag("DATE"):
+                                            saleDate = row[4]
+                                            # print(saleDate.strftime("%Y%m%d"))
+                                            formatedDate = "NA"
+                                            if (saleDate):
+                                                formatedDate = saleDate.strftime("%Y%m%d")
+                                            text(str(formatedDate))
+                                        with tag("INSTRUMENTDATE"):
+                                            saleDate = row[4]
+                                            # print(saleDate.strftime("%Y%m%d"))
+                                            formatedDate = "NA"
+                                            if (saleDate):
+                                                formatedDate = saleDate.strftime("%Y%m%d")
+                                            text(str(formatedDate))
+                                        with tag("NAME"):
+                                            text(str(uuid.uuid4()))
+                                        with tag("TRANSACTIONTYPE"):
+                                            text("Cheque/DD")
+                                        with tag("PAYMENTFAVOURING"):
+                                            if re.search("Receipt", row[7], re.IGNORECASE):
+                                                text(row[2])
+                                            elif re.search("Payment", row[7], re.IGNORECASE):
+                                                text(row[0])
+                                        with tag("UNIQUEREFERENCENUMBER"):
+                                            text(str(uuid.uuid4()))
+                                        with tag("PAYMENTMODE"):
+                                            text("Transacted")
+                                        with tag("BANKPARTYNAME"):
+                                            if re.search("Receipt", row[7], re.IGNORECASE):
+                                                text(row[2])
+                                            elif re.search("Payment", row[7], re.IGNORECASE):
+                                                text(row[0])
+                                        with tag("CHEQUEPRINTED"):
+                                            text("1")
+                                        with tag("AMOUNT"):
+                                            if re.search("Receipt", row[7], re.IGNORECASE):
+                                                text(-round(row[5], 2))
+                                            elif re.search("Payment", row[7], re.IGNORECASE):
+                                                text(round(row[5], 2))
+
+                        with tag("TALLYMESSAGE", ("xmlns:UDF", "TallyUDF")):
+                            with tag("COMPANY"):
+                                with tag("REMOTECMPINFO.LIST", ("MERGE", "Yes")):
+                                    with tag("NAME"):
+                                        text(str(uuid.uuid4()))
+                                    with tag("REMOTECMPNAME"):
+                                        text(fileName)
+                                    with tag("REMOTECMPSTATE"):
+                                        text("Odisha")
+
+    result = indent(
+        doc.getvalue(),
+        indentation = '    ',
+        indent_text = False
+    )
+    print(result)
+
+    with open("bank_transactions"+"_"+fileName+"-new-v1.xml", "w", encoding="utf-8") as f:
+        f.write(result)
+
+    return result
